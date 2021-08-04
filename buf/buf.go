ba000363726b5c0e5607af2466bd11dce2163493
Gert Drapers <gert.drapers@live.com>
2021-08-04 15:00:48 -0700

    add buf support

---
+++ buf/buf.go
@@ -0,0 +1,44 @@
+package buf
+
+import (
+	"strings"
+
+	"github.com/aserto-dev/clui"
+	"github.com/aserto-dev/mage-loot/deps"
+)
+
+type bufArgs struct {
+	args []string
+}
+
+// Arg represents a protoc CLI argument
+type Arg func(*bufArgs)
+
+var (
+	ui = clui.NewUI()
+)
+
+// Run runs the protoc CLI
+func Run(args ...Arg) error {
+	bufArgs := &bufArgs{}
+
+	for _, arg := range args {
+		arg(bufArgs)
+	}
+
+	finalArgs := []string{}
+
+	finalArgs = append(finalArgs, bufArgs.args...)
+
+	ui.Normal().
+		Msg(">>> executing buf " + strings.Join(bufArgs.args, " "))
+
+	return deps.GoDep("buf")(finalArgs...)
+}
+
+// AddArg adds a simple argument.
+func AddArg(arg string) func(*bufArgs) {
+	return func(o *bufArgs) {
+		o.args = append(o.args, arg)
+	}
+}
--- go.mod
+++ go.mod
@@ -5,13 +5,10 @@ go 1.16
 require (
 	github.com/aserto-dev/clui v0.0.1
 	github.com/imdario/mergo v0.3.12
-	github.com/kr/pretty v0.1.0 // indirect
+	github.com/kr/pretty v0.3.0 // indirect
 	github.com/magefile/mage v1.11.0
-	github.com/onsi/ginkgo v1.14.2 // indirect
-	github.com/onsi/gomega v1.10.4
+	github.com/onsi/gomega v1.14.0
 	github.com/pkg/errors v0.9.1
-	github.com/tidwall/gjson v1.7.5
-	golang.org/x/sys v0.0.0-20201009025420-dfb3f7c4e634 // indirect
-	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
-	gopkg.in/yaml.v2 v2.3.0
+	github.com/tidwall/gjson v1.8.1
+	gopkg.in/yaml.v2 v2.4.0
 )
