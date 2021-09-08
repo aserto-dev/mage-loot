c30ba526e55f3fe049eb1adccb6f0a3fad2fa4b0
Gert Drapers <gert.drapers@live.com>
2021-09-07 20:59:59 -0700

    remove obsolete CopyOpenAPI

--- common/openapi.go
+++ common/openapi.go
@@ -2,46 +2,13 @@ package common
 
 import (
 	"encoding/json"
-	"io"
 	"os"
-	"path"
 	"path/filepath"
 	"sort"
 
 	"github.com/aserto-dev/mage-loot/fsutil"
 	"github.com/imdario/mergo"
-	"github.com/magefile/mage/sh"
-	"github.com/pkg/errors"
-	"github.com/tidwall/gjson"
 )
-
-func CreateOpenAPI(repo, outfile string, subServices []string) error {
-	fsutil.EnsureDir(filepath.Dir(outfile))
-
-	jsonStr, err := sh.Output("go", "mod", "edit", "-json")
-	if err != nil {
-		return err
-	}
-
-	version := gjson.Get(jsonStr, "Require.#(Path==\""+repo+"\").Version")
-
-	goModCache, err := sh.Output("go", "env", "GOMODCACHE")
-	if err != nil {
-		return err
-	}
-
-	files := []string{}
-	for _, s := range subServices {
-		files = append(files, path.Join(goModCache, repo+"@"+version.String(), s))
-	}
-
-	sort.Strings(files)
-
-	UI.Normal().
-		Msg("openapi-create")
-
-	return merge(files, outfile)
-}
 
 func MergeOpenAPI(repo, outfile string, subServices []string) error {
 	fsutil.EnsureDir(filepath.Dir(outfile))
@@ -110,71 +77,4 @@ func loadFile(filePath string) (map[string]interface{}, error) {
 	}
 
 	return data, nil
-}
-
-func CopyOpenAPI(repo, service, outfile string) error {
-	jsonStr, err := sh.Output("go", "mod", "edit", "-json")
-	if err != nil {
-		return err
-	}
-
-	version := gjson.Get(jsonStr, "Require.#(Path==\""+repo+"\").Version")
-	if version.String() == "" {
-		return errors.Errorf("no version info found")
-	}
-
-	goModCache, err := sh.Output("go", "env", "GOMODCACHE")
-	if err != nil {
-		return err
-	}
-
-	fp := path.Join(goModCache, repo+"@"+version.String(), "service", service, "openapi.json")
-
-	return copyFile(fp, outfile, true)
-}
-
-func copyFile(src, dst string, overwrite bool) error {
-	const bufferSize int64 = 1024
-
-	srcFileStat, err := os.Stat(src)
-	if err != nil {
-		return err
-	}
-
-	if !srcFileStat.Mode().IsRegular() {
-		return errors.Errorf("%s is not a regular file", src)
-	}
-
-	reader, err := os.Open(src)
-	if err != nil {
-		return err
-	}
-	defer reader.Close()
-
-	_, err = os.Stat(dst)
-	if err == nil && !overwrite {
-		return errors.Errorf("File %s already exists", dst)
-	}
-
-	writer, err := os.Create(dst)
-	if err != nil {
-		return err
-	}
-	defer writer.Close()
-
-	buf := make([]byte, bufferSize)
-	for {
-		n, err := reader.Read(buf)
-		if err != nil && err != io.EOF {
-			return err
-		}
-		if n == 0 {
-			break
-		}
-
-		if _, err := writer.Write(buf[:n]); err != nil {
-			return err
-		}
-	}
-	return err
 }
