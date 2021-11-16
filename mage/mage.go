6ee6ef1a1d00e528e7419a46198e5da9cf744067
mihaibuzgau <mihaibuzgau@gmail.com>
2021-11-16 10:54:05 +0000

    add mage package

---
+++ mage/mage.go
@@ -0,0 +1,67 @@
+package mage
+
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/aserto-dev/clui"
+	"github.com/aserto-dev/mage-loot/common"
+	"github.com/magefile/mage/mage"
+)
+
+// Arg represents a mage argument
+type Arg func(*mageArgs)
+
+var (
+	ui = clui.NewUI()
+)
+
+type mageArgs struct {
+	args []string
+}
+
+// AddArg adds a simple argument.
+func AddArg(arg string) func(*mageArgs) {
+	return func(o *mageArgs) {
+		o.args = append(o.args, arg)
+	}
+}
+
+// Run mage on the specified directory with the given args.
+// the output of the mage command is printed to stdout and the error message to stderr.
+func RunDir(dir string, args ...Arg) error {
+	return mageRun(dir, args...)
+}
+
+// Run mage on the current directory with the given args
+// the output of the mage command is printed to stdout and the error message to stderr.
+func Run(args ...Arg) error {
+	return mageRun(common.WorkDir(), args...)
+}
+
+func mageRun(mageDir string, args ...Arg) error {
+	mageArgs := &mageArgs{}
+
+	for _, arg := range args {
+		arg(mageArgs)
+	}
+
+	invocation := mage.Invocation{
+		Dir:  mageDir,
+		Args: mageArgs.args,
+	}
+
+	invocation.Stderr = os.Stderr
+	invocation.Stdout = os.Stdout
+
+	ui.Normal().
+		Msgf(">>> executing 'mage %s' on %s", strings.Join(mageArgs.args, " "), invocation.Dir)
+
+	exitCode := mage.Invoke(invocation)
+
+	if exitCode != 0 {
+		return fmt.Errorf("mage exited with code %d", exitCode)
+	}
+	return nil
+}
