90984e2a6c9780402c3244c0ddbb991209141efa
mihaibuzgau <mihaibuzgau@gmail.com>
2022-02-16 10:17:32 +0000

    add GenerateWith common func

--- common/generate.go
+++ common/generate.go
@@ -1,6 +1,9 @@
 package common
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/magefile/mage/sh"
 	"github.com/pkg/errors"
 )
@@ -8,10 +11,26 @@ import (
 // Generate runs go generate for the specified paths.
 // If no paths are used, it generates for './...'.
 func Generate(paths ...string) error {
+	return GenerateWith(nil, paths...)
+}
+
+// Generate runs go generate for the specified paths using the specified generation tools.
+// If no paths are used, it generates for './...'.
+func GenerateWith(tools []string, paths ...string) error {
 	UI.Normal().Msg("Generating code.")
 
+	var env map[string]string
+
+	if len(tools) > 0 {
+		env = make(map[string]string)
+		env["PATH"] = os.Getenv("PATH")
+		for _, tool := range tools {
+			env["PATH"] = fmt.Sprintf("%s%s%s", tool, string(os.PathListSeparator), env["PATH"])
+		}
+	}
+
 	if len(paths) == 0 {
-		if err := sh.RunV("go", "generate", "./..."); err != nil {
+		if err := sh.RunWithV(env, "go", "generate", "./..."); err != nil {
 			return errors.Wrap(err, "failed to run go generate for path './...'")
 		}
 
@@ -19,7 +38,7 @@ func Generate(paths ...string) error {
 	}
 
 	for _, path := range paths {
-		if err := sh.RunV("go", "generate", path); err != nil {
+		if err := sh.RunWithV(env, "go", "generate", path); err != nil {
 			return errors.Wrapf(err, "failed to run go generate '%s'", path)
 		}
 	}
