018ec1a8a1f301a199d78cd8f7d7654565f1b2ae
carabasdaniel <dani@aserto.com>
2022-03-01 10:07:12 +0200

    Add gitleaks check to build releaser

--- common/build_releaser.go
+++ common/build_releaser.go
@@ -1,14 +1,25 @@
 package common
 
-import "github.com/aserto-dev/mage-loot/deps"
+import (
+	"github.com/aserto-dev/mage-loot/deps"
+)
 
 // BuildAllReleaser builds all binaries for all OSes and architectures, in preparation for a release.
 func BuildAllReleaser(args ...string) error {
+	err := GitleaksCheck()
+	if err != nil {
+		return err
+	}
 	return deps.GoDep("goreleaser")(append([]string{"build", "--rm-dist"}, args...)...)
 }
 
 // BuildReleaser builds the project.
 func BuildReleaser(args ...string) error {
+	err := GitleaksCheck()
+	if err != nil {
+		return err
+	}
+
 	return deps.GoDep("goreleaser")(append([]string{"build", "--rm-dist", "--snapshot", "--single-target"}, args...)...)
 }
 
